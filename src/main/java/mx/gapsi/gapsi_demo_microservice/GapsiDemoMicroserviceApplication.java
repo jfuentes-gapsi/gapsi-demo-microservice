05c55fd94ecbc3aa64fb0512f6a1d11f2847abf6
jfuentes-gapsi <jfuentes@grupoasesores.com.mx>
2025-10-21 21:39:44 -0600

    JFUENTES
    init

---
+++ src/main/java/mx/gapsi/gapsi_demo_microservice/GapsiDemoMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package mx.gapsi.gapsi_demo_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GapsiDemoMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GapsiDemoMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/gapsi/gapsi_demo_microservice/GapsiDemoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.gapsi.gapsi_demo_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GapsiDemoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
